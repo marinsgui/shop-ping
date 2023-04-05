b8b72d667b0a8aa7f149a1eec2fa58787625036d
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-05 10:38:08 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ShopPing</title>
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  )
+}
