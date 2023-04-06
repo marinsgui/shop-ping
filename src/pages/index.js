b1ceb400f713d21c9e30c5a80a10dada8605b5a6
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-06 12:12:07 -0300

    seção de categorias criada

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Categories from '@/components/Categories'
 import Slider from '@/components/Slider'
 
 import Head from 'next/head'
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       <main>
         <Slider />
+        <Categories />
       </main>
     </>
   )
