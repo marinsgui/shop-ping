89dfe01f079732421b56007e032f991bdad8cd62
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-06 10:07:59 -0300

    barra de anúncios criada

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 
 import Header from '@/components/Header'
+import Announcement from '@/components/Announcement'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Announcement />
       <Header />
       <Component {...pageProps} />
     </>
