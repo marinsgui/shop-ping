84dbe272ab4542bf73074abc8b2479751b40a0e7
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-09 10:28:24 -0300

    firebase adicionado

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNFprOCrT2q-wiMG8Pwnv9o4sGm_TR_hE",
+  authDomain: "shopping-ba3aa.firebaseapp.com",
+  projectId: "shopping-ba3aa",
+  storageBucket: "shopping-ba3aa.appspot.com",
+  messagingSenderId: "244492111813",
+  appId: "1:244492111813:web:686164c8340347861705ac"
+}
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
